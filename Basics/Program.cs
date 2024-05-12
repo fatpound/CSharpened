d00d0a5e18c787a07edfb255b9ce97dfa140533e
fatpound <84704675+fatpound@users.noreply.github.com>
2024-05-12 10:13:11 +0300

    vector, is that you?

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -13,6 +13,18 @@
             
             Console.WriteLine();
             Console.WriteLine("Goodbye, cruel world!");
+
+            List<int> list = new List<int>(); // IDE0028 says I should write [1, 2];
+
+            list.Add(1);
+            list.Add(2);
+
+            Console.WriteLine(list); // System.Collections.Generic.List`1[System.Int32]
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
